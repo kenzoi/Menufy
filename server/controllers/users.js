f69dc69d0d750c3a546477b8eb50ee6520e74655
Kenzo Gomez <kenzo.sgomez@gmail.com>
2021-06-12 17:33:06 +0200

    feat(server): add registerUser handler on controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -8,8 +8,15 @@ module.exports = fp(async function (fastify) {
   async function registerUser (request, reply) {
     try {
       const { username, password, email, restaurantName } = request.body;
-      return await Menu.create({ username, password, email, restaurantName });
-
+      const menu = await Menu.create({ username, password, email, restaurantName });
+      const response = {
+        _id: menu._id,
+        username: menu.username,
+        email: menu.email,
+        restaurantName: menu.restaurantName
+      };
+      reply.code(201);
+      return response;
     } catch (err) {
       console.error(err);
       throw fastify.httpErrors.internalServerError();
