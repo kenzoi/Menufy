9d01e21a9cd5f6659e13097454ebb2da21b2c6c9
Kenzo Gomez <kenzo.sgomez@gmail.com>
2021-06-13 18:42:10 +0200

    feat(server): add deleteGroup handler

--- server/plugins/controllers/menus.js
+++ server/plugins/controllers/menus.js
@@ -112,14 +112,10 @@ module.exports = fp(async function (fastify) {
     try {
       const { menuId } = request.params;
       const { subMenuId, name } = request.body;
-      const menuIdIsValid = mongoose.Types.ObjectId.isValid(menuId);
-      if (!menuIdIsValid) return httpErrors.badRequest('Not a valid menu id');
-      const subMenuIdIsValid = mongoose.Types.ObjectId.isValid(subMenuId);
-      if (!subMenuIdIsValid) return httpErrors.badRequest('Not a valid submenu id');
+      if (!isIdValid(menuId)) return httpErrors.badRequest('Not a valid menu id');
+      if (!isIdValid(subMenuId)) return httpErrors.badRequest('Not a valid submenu id');
       const menuFound = await Menu.findById(menuId).exec();
       if (!menuFound) return httpErrors.notFound('Menu not found this id');
-
-      console.log(menuFound.menu.find((el) => String(el._id) === subMenuId));
       const subMenu = menuFound.menu.find((el) => String(el._id) === subMenuId);
       const subMenuLength = subMenu.groups.push({ name });
       await menuFound.save();
@@ -133,16 +129,16 @@ module.exports = fp(async function (fastify) {
   async function deleteGroup (request) {
     try {
       const { menuId } = request.params;
-      const { _id } = request.body;
-      const menuIdIsValid = mongoose.Types.ObjectId.isValid(menuId);
-      if (!menuIdIsValid) return httpErrors.badRequest('Not a valid menu id');
-      const subMenuIdIsValid = mongoose.Types.ObjectId.isValid(_id);
-      if (!subMenuIdIsValid) return httpErrors.badRequest('Not a valid submenu id');
+      const { _id, subMenuId, name } = request.body;
+      if (!isIdValid(menuId)) return httpErrors.badRequest('Not a valid menu id');
+      if (!isIdValid(subMenuId)) return httpErrors.badRequest('Not a valid submenu id');
+      if (!isIdValid(_id)) return httpErrors.badRequest('Not a valid group id');
       const menuFound = await Menu.findById(menuId).exec();
       if (!menuFound) return httpErrors.notFound('Menu not found this id');
-      const newSubmenu = menuFound.menu.pull(_id); // If there is no element still return the array from DB without warning.
+      const subMenu = menuFound.menu.find((el) => String(el._id) === subMenuId);
+      const newGroups = subMenu.groups.pull(_id);
       await menuFound.save();
-      return newSubmenu;
+      return newGroups;
     } catch (err) {
       console.error(err);
       throw httpErrors.internalServerError();
@@ -161,9 +157,9 @@ module.exports = fp(async function (fastify) {
   });
 
 
-  // Helper function
+  // Helper functions
 
-  function checkId (id) {
+  function isIdValid (id) {
     return mongoose.Types.ObjectId.isValid(id);
   }
 });
